677a2e3fb2760271663d32483874813dd969d4b1
Luca <lcampobasso@gmail.com>
2022-02-28 23:37:40 +0100

    ex3.3 + tests

--- src/Chapter3/Lib.hs
+++ src/Chapter3/Lib.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Chapter3.Lib where
+
+import Prelude hiding (foldr, foldl, product, all)
 
 mapf :: (a -> b) -> [a] -> [b]
 mapf _ [] = []
@@ -96,9 +99,15 @@ tuplesMax = map (\(x,y) -> max x y)
 tuplesMax' :: [(Integer, Integer)] -> [Integer]
 tuplesMax' = map (uncurry max)
 
-foldr' :: (a -> b -> b) -> b -> [a] -> b
-foldr' _ initial [] = initial
-foldr' f initial (x:xs) = f x (foldr' f initial xs)
+-- associates to the right
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr _ initial [] = initial
+foldr f initial (x:xs) = f x (foldr f initial xs)
+
+-- associates to the left
+foldl :: (a -> b -> a) -> a -> [b] -> a
+foldl _ initial [] = initial
+foldl f initial (x:xs) = foldl f (f initial x) xs
 
 -- extending types for custom representation
 data InfNumber a = MinusInf | Number a | PlusInf deriving (Show, Eq)
@@ -109,3 +118,32 @@ infMax x MinusInf = x
 infMax PlusInf _ = PlusInf
 infMax _ PlusInf = PlusInf
 infMax (Number a) (Number b) = Number $ max a b
+
+-- Exercise 3.3 Your first folds
+
+product :: Num a => [a] -> a
+product [] = 1
+product (x:xs) = x * product xs
+
+minimumClient :: [Client a] -> Client a
+minimumClient [] = error "no clients in this list!"
+minimumClient [c] = c
+minimumClient (x:y:xs) = if nameLen x <= nameLen y
+    then minimumClient (x:xs)
+    else minimumClient (y:xs)
+    where nameLen c = length $ ( \case
+            GovOrg{ clientName } -> clientName
+            Company{ clientName } -> clientName
+            Individual{ person = Person{firstName, lastName} } -> firstName ++ " " ++ lastName) c
+
+all :: [Bool] -> Bool
+all [] = error "empty list!"
+all [x] = x
+all (x:xs) = x && all xs
+
+minimumBy :: Ord a => (a -> a) -> [a] -> a
+minimumBy _ [] = error "empty list!"
+minimumBy _ [a] = a
+minimumBy f (x:y:xs) = if f x <= f y
+    then minimumBy f (x:xs)
+    else minimumBy f (y:xs)
--- test/Chapter3/LibSpec.hs
+++ test/Chapter3/LibSpec.hs
@@ -2,31 +2,62 @@ module Chapter3.LibSpec (spec) where
 
 import Test.Hspec
 import Chapter3.Lib
+import Control.Exception (evaluate)
+import Prelude hiding (foldr, foldl, product, all)
 
 spec :: Spec
 spec = do
+    -- clients list fixture
+    let basicList = [1, 2, 3]::[Integer]
+    let fstGovOrg = GovOrg 3 "Russia"
+    let sndGovOrg = GovOrg 4 "Italy"
+    let nonGovOrg = Individual 2 $ Person "a" "b"
+    let clientsList = [fstGovOrg, sndGovOrg, nonGovOrg]::[Client Integer]
+
     describe "doubleMapf" $ do
         it "can map with two elements" $ do
             let lst = [(1, 2), (3, 4), (5, 6)]::[(Integer, Integer)]
             doubleMapf (+) lst `shouldBe` [3, 7, 11]
+
     describe "filterOnes" $ do
         it "can filter basic lists" $ do
             let lst = [1, 1, 2, 3]::[Integer]
             filterOnes lst `shouldBe` [1, 1]
+
     describe "filterANumber" $ do
         it "can filter with whatever number" $ do
             let lst = [1, 3, 5, 4, 5]::[Integer]
             filterANumber 5 lst `shouldBe` [5, 5]
+
     describe "filterGovOrgs" $ do
         it "can filter GovOrgs" $ do
-            let fstGovOrg = GovOrg 3 "Russia"
-            let sndGovOrg = GovOrg 4 "Italy"
-            let nonGovOrg = Individual 2 $ Person "a" "b"
-            let lst = [fstGovOrg, sndGovOrg, nonGovOrg]::[Client Integer]
-            filterGovOrgs lst `shouldBe` [fstGovOrg, sndGovOrg]
+            filterGovOrgs clientsList `shouldBe` [fstGovOrg, sndGovOrg]
+
     describe "foldr'" $ do
-        let lst = [1, 2, 3]::[Integer]
-        it "can fold to right" $ do
-            foldr' (+) 0 lst `shouldBe` 6
+        it "folds to right summing" $ do
+            foldr (+) 0 basicList `shouldBe` 6
+        it "folds to right correctly" $ do
+            foldr (-) 0 basicList `shouldBe` 2
         it "can fold `InfNumber`s with normal numbers" $ do
-            foldr' infMax MinusInf (map Number lst) `shouldBe` (Number 3)
+            foldr infMax MinusInf (map Number basicList) `shouldBe` (Number 3)
+
+    describe "foldl'" $ do
+        it "folds to left correctly" $ do
+            foldl (-) 0 basicList `shouldBe` -6
+
+    describe "product'" $ do
+        it "computes product of a list" $ do
+            let lst = [1, 2, 3]::[Integer]
+            product lst `shouldBe` 6
+
+    describe "minimumClient" $ do
+        it "gets the client with the smallest clientName" $ do
+            minimumClient clientsList `shouldBe` nonGovOrg
+        it "throws an error for an empty list" $ do
+            evaluate( minimumClient [] ) `shouldThrow` errorCall "no clients in this list!"
+
+    describe "minimumBy" $ do
+        it "gives the minimum by first using a transform" $ do
+            minimumBy (\x -> -x) basicList `shouldBe` 3
+
+
